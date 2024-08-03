3e4ff70a173af3e882100374f36b74c8b9a1e9ab
Antonio Ibáñez <am.ibanez.costa@gmail.com>
2024-08-03 16:58:39 +0200

    Implements foreign key with dynamic component based on schema field

--- src/Instances/AccessDataTraits/ColumnForeignTrait.php
+++ src/Instances/AccessDataTraits/ColumnForeignTrait.php
@@ -6,18 +6,35 @@ use Lkt\Factory\Instantiator\Instances\AbstractInstance;
 use Lkt\Factory\Instantiator\Instantiator;
 use Lkt\Factory\Schemas\Exceptions\InvalidSchemaAppClassException;
 use Lkt\Factory\Schemas\Exceptions\SchemaNotDefinedException;
+use Lkt\Factory\Schemas\Schema;
 
 trait ColumnForeignTrait
 {
     /**
-     * @param $type
-     * @param $id
+     * @param string $fieldName
      * @return AbstractInstance|null
      * @throws InvalidSchemaAppClassException
      * @throws SchemaNotDefinedException
      */
-    protected function _getForeignVal($type = '', $id = 0): ?AbstractInstance
+    protected function _getForeignVal($type = '', $id = 0, string $fieldName = ''): ?AbstractInstance
     {
+        if ($fieldName !== '') {
+            $schema = Schema::get(static::COMPONENT);
+            $field = $schema->getField($fieldName);
+
+            if ($field) {
+                $type = $field->getComponent();
+                $dynamicComponentFieldName = $field->getDynamicComponentField();
+                if ($dynamicComponentFieldName !== '') {
+                    $dynamicComponentField = $schema->getField($dynamicComponentFieldName);
+                    $getter = $dynamicComponentField->getGetterForPrimitiveValue();
+                    $dynamicType = $this->{$getter}();
+                    if ($dynamicType !== '') $type = $dynamicType;
+                }
+                $id = $this->_getIntegerVal($fieldName . 'Id');
+            }
+        }
+
         if (!$type || $id <= 0) {
             return null;
         }
@@ -25,14 +42,21 @@ trait ColumnForeignTrait
     }
 
     /**
-     * @param string $type
-     * @param $id
+     * @param string $fieldName
      * @return bool
      * @throws InvalidSchemaAppClassException
      * @throws SchemaNotDefinedException
      */
-    protected function _hasForeignVal(string $type = '', $id = 0): bool
+    protected function _hasForeignVal($type = '', $id = 0, string $fieldName = ''): bool
     {
+        $schema = Schema::get(static::COMPONENT);
+        $field = $schema->getForeignKeyField($fieldName);
+
+        if ($field) {
+            $type = $field->getComponent();
+            $id = $this->_getIntegerVal($fieldName . 'Id');
+        }
+
         return is_object($this->_getForeignVal($type, $id));
     }
 }
