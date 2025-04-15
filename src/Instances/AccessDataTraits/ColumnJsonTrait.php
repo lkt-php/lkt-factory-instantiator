adab0ca2b83b93b5617b1e66ac9af103bc67e59d
Antonio Ibáñez <am.ibanez.costa@gmail.com>
2025-04-15 15:53:40 +0200

    Fix: Parse empty assoc value

--- src/Instances/AccessDataTraits/ColumnJsonTrait.php
+++ src/Instances/AccessDataTraits/ColumnJsonTrait.php
@@ -30,6 +30,7 @@ trait ColumnJsonTrait
         $field = $schema->getField($fieldName);
 
         if ($field->isAssoc()) {
+            if (is_null($r)) $r = json_decode('{}', true);
             if (is_string($r)) $r = json_decode($r, true);
             /** @var array $r */
             return $r;
